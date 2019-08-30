c531f8e3575c9a33cb9674815cd4edd9fd537461
sergeypavlyk <pavlyk.sergeynik@gmail.com>
2019-08-30 14:05:01 +0300

    second section

--- js/script.js
+++ js/script.js
@@ -1,31 +1,51 @@
-const sections = [...document.getElementsByTagName('section')];
-let currentSection = 0;
+// const sections = [...document.getElementsByTagName('section')];
+// let currentSection = 0;
 
-window.addEventListener('wheel', function(e) {
-  e.preventDefault();
+// window.addEventListener('wheel', function(e) {
+//   e.preventDefault();
 
-  (e.deltaY < 0) ? --currentSection: ++currentSection;
+//   (e.deltaY < 0) ? --currentSection: ++currentSection;
 
-  if (currentSection < 0) currentSection = 0;
-  else if (currentSection > (sections.length - 1)) currentSection = (sections.length - 1);
+//   if (currentSection < 0) currentSection = 0;
+//   else if (currentSection > (sections.length - 1)) currentSection = (sections.length - 1);
 
-  scrollToSection(currentSection);
-});
+//   scrollToSection(currentSection);
+// });
 
-function scrollToSection(i) {
-  document.getElementById(sections[i].id).scrollIntoView({
-    behavior: 'smooth'
-  });
-}
+// function scrollToSection(i) {
+//   document.getElementById(sections[i].id).scrollIntoView({
+//     behavior: 'smooth'
+//   });
+// }
 
+
+$(window).scroll(function() {
+     var scroll = $(window).scrollTop();
+     console.log(scroll);
+     var h = $(window).height(); 
+     if (scroll >= 1500) { 
+         $('.rightside img').addClass('active');
+         $('.all h2.white').addClass('active');
+     }
+    })
 
 $(document).ready(function() {
+
 new WOW().init();
 
-$('a[href=#top]').click(function () {
-    $('body,html').animate({
-        scrollTop: 0
-    }, 400);
-    return false;
+$('#top').on("click",function() {
+    $('body').animate({ scrollTop: 0 }, 'slow');
 });
+
+$('.play').on('click', function(){
+    $('.dopsvideo')[0].play();
+    $('.play').css('display', 'none');
+})
+if ($(".dopsvideo").get(0).played){
+    $('.dopsvideo').on('click', function(){
+        $(".dopsvideo").get(0).pause();
+        $('.play').css('display', '');
+    })
+}
+
 });
